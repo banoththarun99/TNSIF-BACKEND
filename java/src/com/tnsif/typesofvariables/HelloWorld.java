54195b309337c0de7b5cfbda22aa5064cdab87dc
PAVAN SABAVAT <sabavatpavan.50@gmail.com>
2025-05-09 10:09:07 +0530

    Just a check

---
+++ java/src/com/tnsif/typesofvariables/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.tnsif.typesofvariables;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI Da..");
+		
+	}
+
+}
